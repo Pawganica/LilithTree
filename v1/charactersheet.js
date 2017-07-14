b0c20f4b605c74d6baf29e17eaccc2741d152efc
Pawganica <pawganica@gmail.com>
2017-07-14 07:55:25 +0200

    LT Begin

---
+++ v1/charactersheet.js
@@ -0,0 +1,19 @@
+var cs = {
+	
+	
+	set Agility(x) {
+		localStorage.cs_Agility = x;
+	}
+	
+	get Agility() {
+		return localStorage.cs_Agility;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ v1/core.js
@@ -0,0 +1,42 @@
+var charSheet;
+var sceneBox;
+var actionBox;
+
+//TODO: localStorage wrapper
+//TODO: step cycle
+//TODO: global event stream
+//TODO: location action wrapper
+//TODO: location description wrapper
+//TODO: Missed event wrapper - call module functions for missed events
+
+//localStorage.Position = "TestDungeon";
+
+var position = {
+	
+	Get: function() {
+		return localStorage.Position;
+	},
+	
+	Set: function(newPosition) {
+		localStorage.Position = newPosition;
+	}
+	
+
+}
+
+function mainInit()
+{
+	charSheet = document.getElementById("charsheet");
+	sceneBox = document.getElementById("scenebox");
+	actionBox = document.getElementById("actionbox");
+	
+	charSheet.innerHTML = "Test";
+	
+	var initCurrentPosition = position.Get();
+	callFunction(initCurrentPosition);
+}
+
+function callFunction(functionName)
+{
+	window[functionName]();
+}
